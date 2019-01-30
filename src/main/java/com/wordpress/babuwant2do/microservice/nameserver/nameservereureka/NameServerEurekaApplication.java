dfe219004fbc09e4b523c8f3e4d837ee2b353cc9
Mohammed Ali <mohammedali@Mohammeds-MacBook-Pro.local>
2019-01-30 17:07:18 +0100

    init code from spring SPRING INITIALIZR

---
+++ src/main/java/com/wordpress/babuwant2do/microservice/nameserver/nameservereureka/NameServerEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.wordpress.babuwant2do.microservice.nameserver.nameservereureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NameServerEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NameServerEurekaApplication.class, args);
+	}
+
+}
+
