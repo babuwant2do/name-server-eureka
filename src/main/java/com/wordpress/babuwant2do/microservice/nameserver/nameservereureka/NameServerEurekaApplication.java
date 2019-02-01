b2ef695ca0078b1af45c888e819377301a26f46d
Mohammed Ali <mohammedali@Mohammeds-MacBook-Pro.local>
2019-02-01 17:09:26 +0100

    implement simple Eureka Naming server

--- src/main/java/com/wordpress/babuwant2do/microservice/nameserver/nameservereureka/NameServerEurekaApplication.java
+++ src/main/java/com/wordpress/babuwant2do/microservice/nameserver/nameservereureka/NameServerEurekaApplication.java
@@ -2,8 +2,10 @@ package com.wordpress.babuwant2do.microservice.nameserver.nameservereureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
+@EnableEurekaServer
 public class NameServerEurekaApplication {
 
 	public static void main(String[] args) {
